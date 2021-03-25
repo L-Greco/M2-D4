732b7e876e0b67578dc9d55c9e86adf958b11c8d
L-Greco <makaronaskonstandinos@gmail.com>
2021-03-25 17:58:07 +0200

    Ready for the ending

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
 let arrayOfNames = []
+let name1=""
+let name2=""
 const addNames = function () {
     let name = document.getElementsByTagName("input")[0].value
     let listName = document.createElement("li")
@@ -12,12 +14,35 @@ let arrayOfPairs = []
 const makingOfTheLists = function () {  
     let list = document.getElementsByTagName("ol")[0]
     let numberOfLists = document.getElementsByTagName("select")[0].value
+    let existedLists = document.querySelectorAll("ol li")
+    let numberOfExistedLists = document.querySelectorAll("ol li").length
+    for (i=0;i<numberOfExistedLists;i++) {
+        list.removeChild(existedLists[i])
+    }
     for ( i=0;i< numberOfLists;i++) {
     let listName = document.createElement("li")
+    listName.innerText = `${name1} + ${name2}`
     listName.classList.add("list-group-item")
     list.appendChild(listName)
-    arrayOfPairs.push("")
-    
+    arrayOfPairs.push("")   
 }
+}
+
+const pairsGeneration = function () {
+    let counter = 0
+let numberOfUls = document.querySelectorAll("ul li").length
+let uls = document.querySelectorAll("ul li")
+let ul = document.getElementsByTagName("ul")[0]
+let lis = document.querySelectorAll("ol li")
+let ol = document.getElementsByTagName("ol")[0]
+let rng1 = Math.round(Math.random()*numberOfUls-1)
+ul.removeChild(uls[rng1])
+ol.replaceChild(lis[counter],uls[rng1])
+let rng2 = Math.round(Math.random()*numberOfUls-1)
+ul.removeChild(uls[rng2])
+
+
+counter ++
+
 }
 
